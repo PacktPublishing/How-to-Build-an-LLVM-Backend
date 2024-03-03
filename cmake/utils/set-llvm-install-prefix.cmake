debbe052ce21eaf461a3a57684c417e8999b2367
Quentin Colombet <quentin.colombet@gmail.com>
2024-03-03 01:04:05 +0100

    [CH3] Fix find_package for LLVM

--- cmake/utils/set-llvm-install-prefix.cmake
+++ cmake/utils/set-llvm-install-prefix.cmake
@@ -1,24 +1,10 @@
-# Helper cmake file to set the include and library search paths.
-# We could use find_package, but this is something more custom.
+# Helper cmake file to set the include and library search paths of LLVM.
+find_package(LLVM REQUIRED CONFIG)
 
-if(NOT DEFINED LLVM_INSTALL_PREFIX)
-  message(FATAL_ERROR
- "You must set LLVM_INSTALL_PREFIX.\n"
- "This variable must point to your LLVM installation.\n"
- "To build and install LLVM, "
- "Follow Chapter 1's steps, while setting the CMAKE_INSTALL_PREFIX variable.\n"
- "Then build the 'install' target.\n"
- "Finally feed your install prefix pass to this cmake.\n")
-endif()
+message(STATUS "Found LLVM ${LLVM_PACKAGE_VERSION}")
+message(STATUS "Using LLVMConfig.cmake in: ${LLVM_DIR}")
 
-# Setup the search paths for LLVM related things.
-# includes.
-target_include_directories(${CURR_TARGET} PUBLIC
-   ${LLVM_INSTALL_PREFIX}/include
-)
-
-# libaries.
-target_link_directories(${CURR_TARGET} PUBLIC
-   ${LLVM_INSTALL_PREFIX}/lib
-)
+include_directories(${LLVM_INCLUDE_DIRS})
+separate_arguments(LLVM_DEFINITIONS_LIST NATIVE_COMMAND ${LLVM_DEFINITIONS})
+add_definitions(${LLVM_DEFINITIONS_LIST})
 
