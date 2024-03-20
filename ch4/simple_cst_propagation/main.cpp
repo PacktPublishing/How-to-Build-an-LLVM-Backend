3e71423cc1939a946216f7e7c269861040cd67d7
Quentin Colombet <quentin.colombet@gmail.com>
2024-03-20 20:27:44 +0100

    [CH4] Add implementation for simple constant propagation
    Only handle binary operation.
    Provide a way to feed input IR file

--- ch4/simple_cst_propagation/main.cpp
+++ ch4/simple_cst_propagation/main.cpp
@@ -3,8 +3,10 @@
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Verifier.h"
+#include "llvm/IRReader/IRReader.h" // For parseIRFile.
 #include "llvm/Support/Debug.h"     // For errs().
 #include "llvm/Support/SourceMgr.h" // For SMDiagnostic.
+#include "llvm/Transforms/Utils/Cloning.h" // For CloneFunction.
 
 using namespace llvm;
 
@@ -22,49 +24,114 @@ bool checkFunctionCorrectness(llvm::Function &Res) {
   return true;
 }
 
+// Default input in case no file was provided.
 const char *InputIR =
-    "@global = external global ptr, align 8\n"
-    "@other_global = local_unnamed_addr global ptr @global, align 8\n"
-    "\n"
-    "define signext i8 @foo() {\n"
+    "define i32 @foo(i32 noundef %arg) {\n"
     "bb:\n"
-    "  %i = load ptr, ptr @global, align 8\n"
-    "  %i1 = load i8, ptr %i, align 1\n"
-    "  ret i8 %i1\n"
+    "  %i = shl i32 5, 3\n"
+    "  %i1 = icmp ne i32 %arg, 0\n"
+    "  br i1 %i1, label %bb2, label %bb4\n"
+    "\n"
+    "bb2:                                              ; preds = %bb\n"
+    "  %i3 = sdiv i32 %i, 5\n"
+    "  br label %bb6\n"
+    "\n"
+    "bb4:                                              ; preds = %bb\n"
+    "  %i5 = or i32 %i, 3855\n"
+    "  br label %bb6\n"
+    "\n"
+    "bb6:                                              ; preds = %bb4, %bb2\n"
+    "  %.0 = phi i32 [ %i3, %bb2 ], [ %i5, %bb4 ]\n"
+    "  ret i32 %.0\n"
     "}\n"
     "\n"
-    "define signext i8 @bar() {\n"
+    "define i32 @bar(i32 noundef %arg) {\n"
     "bb:\n"
-    "  %i = load ptr, ptr @global, align 8\n"
-    "  %i1 = load i8, ptr %i, align 1\n"
-    "  ret i8 %i1\n"
+    "  %i = shl i32 -1, 3\n"
+    "  %i1 = icmp ne i32 %arg, 0\n"
+    "  br i1 %i1, label %bb2, label %bb4\n"
+    "\n"
+    "bb2:                                              ; preds = %bb\n"
+    "  %i3 = udiv i32 %i, 3\n"
+    "  br label %bb6\n"
+    "\n"
+    "bb4:                                              ; preds = %bb\n"
+    "  %i5 = or i32 %i, 3855\n"
+    "  br label %bb6\n"
+    "\n"
+    "bb6:                                             ; preds = %bb4, %bb2\n"
+    "  %.0 = phi i32 [ %i3, %bb2 ], [ %i5, %bb4 ]\n"
+    "  %i7 = add i32 %.0, 1\n"
+    "  ret i32 %i7\n"
     "}\n";
 
-int main() {
+int main(int argc, char **argv) {
   LLVMContext Context;
   SMDiagnostic Err;
-  std::unique_ptr<Module> MyModule = parseAssemblyString(InputIR, Err, Context);
-  Function *BarFunc = MyModule->getFunction("bar");
-
-  bool solutionDidSomething =
-      solutionConstantPropagation(*BarFunc);
-  bool solutionIsCorrect = checkFunctionCorrectness(*BarFunc);
-
-  bool yourTurnDidSomething =
-      myConstantPropagation(*BarFunc);
-  bool yourTurnIsCorrect = checkFunctionCorrectness(*BarFunc);
-
-  if (!(solutionIsCorrect && yourTurnIsCorrect))
+  std::unique_ptr<Module> MyModule;
+  // To be able to play with the optimization a little bit,
+  // support a mode where you can feed your own IR files.
+  if (argc == 2) {
+    outs() << "Reading module from '" << argv[1] << "'\n";
+    MyModule = parseIRFile(argv[1], Err, Context);
+  } else {
+    MyModule = parseAssemblyString(InputIR, Err, Context);
+  }
+  if (!MyModule) {
+    errs() << "Unable to build module\n";
     return -1;
-
-  if (solutionDidSomething) {
-    if (!yourTurnDidSomething)
-      outs() << "The solution managed to optimize something but you did not\n";
-    else
-      outs() << "Both your solution and the provided solution did something, which one is better?\n";
-  } else if (yourTurnDidSomething) {
-    outs() << "Nice! You managed to optimize something whereas the provided solution did not!";
   }
 
-  return 0;
+  // Put all the functions in a worklist and process them afterwards.
+  // We do that because we clone the functions on the fly to have the
+  // same input to both the reference implementation and the user provided
+  // solution and we don't want to process cloned functions afterwards and
+  // clone them and so on.
+  SmallVector<Function *> Worklist;
+  for (Function &Func : *MyModule)
+    Worklist.push_back(&Func);
+
+  // Map for the cloned functions.
+  ValueToValueMapTy VMap;
+  bool hadError = false;
+  for (Function *Func : Worklist) {
+    outs() << "Processing function '" << Func->getName() << '\n';
+    Func->print(outs());
+
+    // Clone the function before the optimization to make sure each
+    // implementation sees the same input.
+    auto *ClonedFunc = CloneFunction(Func, VMap);
+
+    outs() << "\n\n## Reference implementation\n";
+    bool solutionDidSomething = solutionConstantPropagation(*ClonedFunc);
+    bool solutionIsCorrect = checkFunctionCorrectness(*ClonedFunc);
+
+    outs() << "\n\n## Your implementation\n";
+    bool yourTurnDidSomething = myConstantPropagation(*Func);
+    bool yourTurnIsCorrect = checkFunctionCorrectness(*Func);
+
+    if (!(solutionIsCorrect && yourTurnIsCorrect)) {
+      hadError = true;
+      errs() << "Solution does not verify:\n"
+                "- provided implementation("
+             << (solutionIsCorrect ? "passed" : "failed")
+             << ")\n"
+                " - your implementation("
+             << (yourTurnIsCorrect ? "passed" : "failed") << ")\n";
+    }
+
+    if (solutionDidSomething) {
+      if (!yourTurnDidSomething)
+        outs()
+            << "The solution managed to optimize something but you did not\n";
+      else
+        outs() << "Both your solution and the provided solution did something, "
+                  "which one is better?\n";
+    } else if (yourTurnDidSomething) {
+      outs() << "Nice! You managed to optimize something whereas the provided "
+                "solution did not!";
+    }
+  }
+
+  return !hadError;
 }
--- ch4/simple_cst_propagation/solution/populate_function.cpp
+++ ch4/simple_cst_propagation/solution/populate_function.cpp
@@ -1,14 +1,16 @@
-#include "llvm/CodeGen/GlobalISel/MachineIRBuilder.h"
-#include "llvm/CodeGen/MachineFrameInfo.h" // For CreateStackObject.
-#include "llvm/CodeGen/MachineFunction.h"
-#include "llvm/CodeGen/MachineMemOperand.h" // For MachinePointerInfo.
-#include "llvm/CodeGen/MachineModuleInfo.h"
-#include "llvm/CodeGen/MachineRegisterInfo.h"
-#include "llvm/CodeGen/Register.h"
-#include "llvm/CodeGen/TargetOpcodes.h"
-#include "llvm/CodeGenTypes/LowLevelType.h" // For LLT.
+#include "llvm/ADT/APInt.h"
+#include "llvm/ADT/PostOrderIterator.h" // For ReversePostOrderTraversal.
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/CFG.h"       // To instantiate RPOTraversal.
+#include "llvm/IR/Constants.h" // For ConstantInt.
 #include "llvm/IR/Function.h"
-#include "llvm/IR/InstrTypes.h" // For ICMP_EQ.
+#include "llvm/IR/InstrTypes.h" // For BinaryOperator, etc.
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/Support/Debug.h" // For errs().
+
+#include <optional>
 
 using namespace llvm;
 
@@ -31,6 +33,129 @@ using namespace llvm;
 // w0 and w1 are given as argument of this Function.
 //
 // The local variable named var is expected to live on the stack.
+
+static Value *visitBinary(Instruction &Instr, LLVMContext &Ctxt,
+                          std::optional<APInt> (*Computation)(const APInt &,
+                                                              const APInt &)) {
+  assert(isa<BinaryOperator>(Instr) && "This is meant for binary instruction");
+
+  auto *LHS = dyn_cast<ConstantInt>(Instr.getOperand(0));
+  auto *RHS = dyn_cast<ConstantInt>(Instr.getOperand(1));
+  if (!LHS || !RHS)
+    return nullptr;
+
+  // FIXME: Technically this API is not precise enough because we may want to
+  // produce poison value for e.g., a division by zero.
+  std::optional<APInt> Res = Computation(LHS->getValue(), RHS->getValue());
+  if (!Res.has_value())
+    return nullptr;
+  auto NewConstant = ConstantInt::get(Ctxt, *Res);
+  return NewConstant;
+}
+
 bool solutionConstantPropagation(Function &Foo) {
-  return true;
+  LLVMContext &Ctxt = Foo.getParent()->getContext();
+  bool MadeChanges = false;
+
+  ReversePostOrderTraversal<Function *> RPOT(&Foo);
+  for (BasicBlock *BB : RPOT) {
+    // Early increment to be able to remove the instruction that we replaced
+    // on-the-fly. The alternative is to accumulate the instructions to remove
+    // in a worklist and delete them afterwards.
+    for (Instruction &Instr : make_early_inc_range(*BB)) {
+      Value *NewConstant = nullptr;
+      switch (Instr.getOpcode()) {
+      case Instruction::Add:
+        NewConstant = visitBinary(
+            Instr, Ctxt,
+            [](const APInt &A, const APInt &B) -> std::optional<APInt> {
+              return A + B;
+            });
+        break;
+      case Instruction::Sub:
+        NewConstant = visitBinary(
+            Instr, Ctxt,
+            [](const APInt &A, const APInt &B) -> std::optional<APInt> {
+              return A - B;
+            });
+        break;
+      case Instruction::Mul:
+        NewConstant = visitBinary(
+            Instr, Ctxt,
+            [](const APInt &A, const APInt &B) -> std::optional<APInt> {
+              return A * B;
+            });
+        break;
+      case Instruction::SDiv:
+        NewConstant = visitBinary(
+            Instr, Ctxt,
+            [](const APInt &A, const APInt &B) -> std::optional<APInt> {
+              if (B.isZero())
+                return std::nullopt;
+              return A.sdiv(B);
+            });
+        break;
+      case Instruction::UDiv:
+        NewConstant = visitBinary(
+            Instr, Ctxt,
+            [](const APInt &A, const APInt &B) -> std::optional<APInt> {
+              if (B.isZero())
+                return std::nullopt;
+              return A.udiv(B);
+            });
+        break;
+      case Instruction::Shl:
+        NewConstant = visitBinary(
+            Instr, Ctxt,
+            [](const APInt &A, const APInt &B) -> std::optional<APInt> {
+              return A.shl(B);
+            });
+        break;
+      case Instruction::LShr:
+        NewConstant = visitBinary(
+            Instr, Ctxt,
+            [](const APInt &A, const APInt &B) -> std::optional<APInt> {
+              return A.lshr(B);
+            });
+        break;
+      case Instruction::AShr:
+        NewConstant = visitBinary(
+            Instr, Ctxt,
+            [](const APInt &A, const APInt &B) -> std::optional<APInt> {
+              return A.ashr(B);
+            });
+        break;
+      case Instruction::And:
+        NewConstant = visitBinary(
+            Instr, Ctxt,
+            [](const APInt &A, const APInt &B) -> std::optional<APInt> {
+              return A & B;
+            });
+        break;
+      case Instruction::Or:
+        NewConstant = visitBinary(
+            Instr, Ctxt,
+            [](const APInt &A, const APInt &B) -> std::optional<APInt> {
+              return A | B;
+            });
+        break;
+      case Instruction::Xor:
+        NewConstant = visitBinary(
+            Instr, Ctxt,
+            [](const APInt &A, const APInt &B) -> std::optional<APInt> {
+              return A ^ B;
+            });
+        break;
+
+      default:
+        break;
+      }
+      if (NewConstant) {
+        Instr.replaceAllUsesWith(NewConstant);
+        Instr.eraseFromParent();
+        MadeChanges = true;
+      }
+    }
+  }
+  return MadeChanges;
 }
