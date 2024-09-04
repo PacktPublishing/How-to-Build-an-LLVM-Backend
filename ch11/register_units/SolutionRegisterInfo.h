d317aeba54db9982530eebe6a4e23ba8a432561d
Quentin Colombet <quentin.colombet@gmail.com>
2024-09-04 22:05:17 +0200

    [CH11] Implement the actual class for the RegisterInfos
    The implementation is just good enough to get through compilation
    and linking.

--- ch11/register_units/SolutionRegisterInfo.h
+++ ch11/register_units/SolutionRegisterInfo.h
@@ -1,10 +1,41 @@
 #ifndef __SOLUTIONREGISTERINFO_H__
 #define __SOLUTIONREGISTERINFO_H__
 
+#include "llvm/ADT/BitVector.h"
+#include "llvm/CodeGen/MachineBasicBlock.h"
 #include <cstdint>
 
 #define GET_REGINFO_HEADER
 #define GET_REGINFO_ENUM // Technically this one belongs to MC.
 #include "SolutionGenRegisterInfo.inc"
+
+namespace llvm {
+class MachineFunction;
+
+class SolutionRegisterInfo : public SolutionGenRegisterInfo {
+public:
+  SolutionRegisterInfo() : SolutionGenRegisterInfo(Register()) {}
+
+  BitVector getReservedRegs(const MachineFunction &MF) const override {
+    return BitVector();
+  }
+
+  const MCPhysReg *
+  getCalleeSavedRegs(const MachineFunction *MF) const override {
+    return nullptr;
+  }
+
+  bool eliminateFrameIndex(MachineBasicBlock::iterator II, int SPAdj,
+                           unsigned FIOperandNum,
+                           RegScavenger *RS = nullptr) const override {
+    return false;
+  }
+
+  Register getFrameRegister(const MachineFunction &MF) const override {
+    return Register();
+  }
+};
+
+} // end namespace llvm.
 
 #endif
